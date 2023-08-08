148e194dd7b4a1f8b1da9190115776b5e82355fa
emondewan00 <dewanmohammademon@gmail.com>
2023-08-08 14:48:48 +0600

    redux setup done

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { increment, decrement } from "../../../redux/features/counter";
 
 const Home = () => {
-    return (
-        <div>
-            <h2>This is Home Page</h2>
+  const count = useSelector((state) => state.count);
+  const dispatch = useDispatch();
+
+  const incrementHandler = () => {
+    dispatch(increment());
+  };
+  const decrementHandler = () => {
+    dispatch(decrement());
+  };
+  return (
+    <div>
+      <div className="bg-gray-50 shadow-xl rounded max-w-sm mx-auto my-10">
+        <h1 className="text-4xl text-center">Hallo programars</h1>
+        <h1 className="text-3xl font-bold text-center pt-4 ">Count {count.count}</h1>
+        <div className="card-body  ">
+          <button className="btn btn-secondary" onClick={incrementHandler}>
+            Increment
+          </button>
+          <button className="btn btn-secondary" onClick={decrementHandler}>
+            Decrement
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Home;
