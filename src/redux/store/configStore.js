1df9456aaa9107bcdf2535d6e1c444979940a2d2
emondewan00 <dewanmohammademon@gmail.com>
2023-08-08 14:53:27 +0600

    add some comment

--- src/redux/store/configStore.js
+++ src/redux/store/configStore.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterSlice from "../features/counter";
+//set up redux store
 const store = configureStore({
   reducer: {
     count: counterSlice.reducer,
